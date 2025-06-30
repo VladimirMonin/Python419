4d9473415c70f37c9909414c2eebfed28128c4ea
VladimirMonin <vladimir.monin2016@yandex.ru>
2025-06-30 23:49:18 +0500

    todo: lesson_18 - обработка событий

--- lesson_17/todo_17.js
+++ lesson_17/todo_17.js
@@ -12,11 +12,72 @@ function askUser() {
   return taskText;
 }
 
+// 7. Функция которая создает кнопку и вешает логику удаления карточки
+function createDeleteButton(taskCard) {
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "❌";
+  deleteButton.addEventListener("click", () => {
+    taskCard.remove();
+    console.log(`Карточка удалена`);
+  });
+  return deleteButton;
+}
+
+// 8. Функция создает кнопку редактировать текст карточки и вешает логику редактирования текста
+function createEditButton(taskTextDiv) {
+  const editButton = document.createElement("button");
+  editButton.textContent = "✏️";
+  editButton.addEventListener("click", () => {
+    const newText = prompt(
+      "Введите новый текст задачи:",
+      taskTextDiv.textContent
+    );
+    //   Перезапись
+    taskTextDiv.textContent = newText;
+    console.log(`Текст карточки изменен`);
+  });
+  return editButton;
+}
+
+// 9. Функция создает кнопку завершить задачу и вешает логику перемещения карточки в <div class="done-container"></div>
+function createDoneButton(taskCard) {
+  const doneButton = document.createElement("button");
+  doneButton.textContent = "✅";
+  doneButton.addEventListener("click", () => {
+    const doneContainer = document.querySelector(".done-container");
+    doneContainer.appendChild(taskCard);
+    console.log(`Карточка перемещена в выполненные`);
+  });
+  return doneButton;
+}
+
 // 2. Функция создает карточку задачи
 function createTaskCard(taskText) {
   const taskCard = document.createElement("div");
   taskCard.classList.add("task");
-  taskCard.textContent = taskText;
+
+  //   СОЗДАНИЕ 2 DIV - для текста и для кнопок div.task-text  div.task-buttons
+  const taskTextDiv = document.createElement("div");
+  taskTextDiv.classList.add("task-text");
+
+  const taskButtonsDiv = document.createElement("div");
+  taskButtonsDiv.classList.add("task-buttons");
+
+  taskTextDiv.textContent = taskText;
+  //   ДОБАВЛЕНИЕ ВСЕХ КНОПОК В КАРТОЧКУ
+  //    Создаем кнопку удаления и добавляем её в карточку
+  const deleteButton = createDeleteButton(taskCard);
+  taskButtonsDiv.appendChild(deleteButton);
+  //    Создаем кнопку редактирования и добавляем её в карточку
+  const editButton = createEditButton(taskTextDiv);
+  taskButtonsDiv.appendChild(editButton);
+  //    Создаем кнопку выполнения и добавляем её в карточку
+  const doneButton = createDoneButton(taskCard);
+  taskButtonsDiv.appendChild(doneButton);
+
+  // Добавляем контейнер текста и контейнер кнопок
+  taskCard.appendChild(taskTextDiv);
+  taskCard.appendChild(taskButtonsDiv);
   return taskCard;
 }
 
@@ -25,9 +86,12 @@ function addTaskToList(container, taskCard) {
   container.appendChild(taskCard);
 }
 
-// 4. Главная функция
-function main() {
+// 4. Главная функция создания задачи
+function createTaskItem() {
   const taskText = askUser();
   const taskCard = createTaskCard(taskText);
   addTaskToList(toDoContainer, taskCard);
 }
+
+// 5. Обработчик события для кнопки "Создать задачу"
+createTask.addEventListener("click", createTaskItem);
